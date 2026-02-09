232c095932c58b4335215ff27b6f0fbd27e157db
DhanalakshmiN20 <vvce22cse0143@vvce.ac.in>
2026-02-09 22:03:10 +0530

    Setup Spring Boot project with JPA and PostgreSQL

---
+++ src/main/java/com/edutech/backend/EdutechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechBackendApplication.class, args);
+	}
+
+}
