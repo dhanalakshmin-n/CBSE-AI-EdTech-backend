ad80ff216d34ac9f8e209f244caef002928607ff
dhanalakshmi-n <dhanalakshmi.n@webknot.in>
2026-02-11 23:12:23 +0530

    added student quiz service APIs

--- src/main/java/com/edutech/backend/quiz/service/QuizService.java
+++ src/main/java/com/edutech/backend/quiz/service/QuizService.java
@@ -6,6 +6,9 @@ import com.edutech.backend.quiz.entity.Quiz;
 import com.edutech.backend.quiz.repository.QuizRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.edutech.backend.user.entity.User;
+import com.edutech.backend.user.repository.UserRepository;
+
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class QuizService {
 
     private final QuizRepository quizRepository;
+    private final UserRepository userRepository;
 
     public List<Quiz> getQuizzesByClass(Integer classLevel) {
         return quizRepository.findByClassLevel(classLevel);
@@ -65,6 +69,71 @@ public Quiz updateQuiz(Long id, UpdateQuizRequest request) {
 public List<Quiz> searchQuizzes(String keyword) {
     return quizRepository.findByQuestionTextContainingIgnoreCase(keyword);
 }
+
+
+//student quiz service
+
+
+public List<Quiz> getQuizzesForStudent(Long userId) {
+
+    User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+    if (user.getStudentClass() == null) {
+        throw new RuntimeException("Student class not assigned");
+    }
+
+    Integer classLevel = Integer.parseInt(user.getStudentClass());
+
+    return quizRepository.findByClassLevel(classLevel);
+}
+
+//to check the class level of the quiz and the student class level
+public Quiz getQuizForStudent(Long userId, Long quizId) {
+
+    User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+    if (user.getStudentClass() == null) {
+        throw new RuntimeException("Student class not assigned");
+    }
+
+    Integer studentClassLevel = Integer.parseInt(user.getStudentClass());
+
+    Quiz quiz = quizRepository.findById(quizId)
+            .orElseThrow(() -> new RuntimeException("Quiz not found"));
+
+    if (!quiz.getClassLevel().equals(studentClassLevel)) {
+        throw new RuntimeException("Unauthorized access to quiz");
+    }
+
+    return quiz;
+}
+
+//attempt quiz
+
+public boolean attemptQuiz(Long userId, Long quizId, String selectedAnswer) {
+
+    User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+    if (user.getStudentClass() == null) {
+        throw new RuntimeException("Student class not assigned");
+    }
+
+    Integer studentClassLevel = Integer.parseInt(user.getStudentClass());
+
+    Quiz quiz = quizRepository.findById(quizId)
+            .orElseThrow(() -> new RuntimeException("Quiz not found"));
+
+    // Security check
+    if (!quiz.getClassLevel().equals(studentClassLevel)) {
+        throw new RuntimeException("Unauthorized access to quiz");
+    }
+
+    return quiz.getCorrectAnswer().equalsIgnoreCase(selectedAnswer);
+}
+
 
 
 }
